e0da728e5594967c8d2160d723b77d058bedf8c0
Roberto Nunes <jrvn@cin.ufpe.br>
2022-03-27 17:51:04 -0300

    q

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from socket import *
 
-#def carry_around_add(a, b):
- #   c = a + b
-  #  return (c & 0xffff) + (c >> 16)
-#
-#def checksum(msg):
-#    s = 0
-#    for i in range(0, len(msg), 2):
-#        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
-#        s = carry_around_add(s, w)
-#    return ~s & 0xffff
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+def checksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
 
 serverName = 'localhost'
 serverPort = 12000
--- server.py
+++ server.py
@@ -23,22 +23,22 @@ class Cliente:
     def getvalor(self):
         return self.valor_total
 
+class Mesa:
+    def __init__(self, n_mesa):
+        self.numero = n_mesa
+        self.clientes = []
+
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
-Mesas = {
-    'numero': '',
-    'clientes': [],
-    'total_mesa': 0
-}
+serverSocket.bind(('', serverPort))Mesas = []
 
 comandos = dict(zip([1, 2, 3, 4, 5, 6], ['cardápio', 'pedir', 'conta individual', 'pagar', 'levantar', 'conta da mesa']))
 
 while 1:
     comando, clientAddress = serverSocket.recvfrom(2048)
     if comando.decode() == 'chefia':
-        #criar thread para cada cliente
+
 
 
 
