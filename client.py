aa1c83fcb8c979fae1f0328d58781678f971a962
Roberto Nunes <jrvn@cin.ufpe.br>
2022-03-27 21:52:43 -0300

    b

--- client.py
+++ client.py
@@ -1,26 +1,39 @@
 from datetime import datetime
 from socket import *
 
-def carry_around_add(a, b):
-    c = a + b
-    return (c & 0xffff) + (c >> 16)
+def checksum(stri) :
+    a=0
+    stri = str(stri)
+    for c in stri:
+       a += ord(c)
 
-def checksum(msg):
-    s = 0
-    for i in range(0, len(msg), 2):
-        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
-        s = carry_around_add(s, w)
-    return ~s & 0xffff
+    return a%2
+
 
 serverName = 'localhost'
 serverPort = 12000
+bufferSize = 1024
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-comando = '0'
-while(comando != '6' or comando != 'conta da mesa'):
+cmd = ''
+
+while cmd != 'conta da mesa':
     apresentacao = f'{datetime.now().hour}:{datetime.now().minute}'
-    comando = input(f'{apresentacao} Cliente:')
-    clientSocket.sendto(comando.encode(), (serverName, serverPort))
-    response, serverAddress = clientSocket.recvfrom(2048)
+    cmd = input(f'{apresentacao} Cliente:')
+    comando = str.encode(cmd)
+    clientSocket.sendto(comando, (serverName, serverPort))
+    clientSocket.sendto(str.encode(str(checksum(comando))), (serverName, serverPort))
+    response, serverAddress = clientSocket.recvfrom(bufferSize)
+    ok = 'ok'
+
+    while response.decode() != 'ok':
+        clientSocket.sendto(comando, (serverName, serverPort))
+        clientSocket.sendto(str.encode(str(checksum(comando))), (serverName, serverPort))
+        response, serverAddress = clientSocket.recvfrom(bufferSize)
+        checkedResponse = clientSocket.recvfrom(bufferSize)
+
+    if response.decode() == 'ok':
+        clientSocket.sendto(comando, (serverName, serverPort))
     print(f'{apresentacao} CInToFome:', response.decode())
+
 
 clientSocket.close()
--- server.py
+++ server.py
@@ -29,30 +29,64 @@ class Mesa:
     def getvalues(self):
         return self.numero
 
+def checksum(stri) :
+    a=0
+    stri = str(stri)
+    for c in stri:
+       a += ord(c)
+
+    return a%2
 
 serverPort = 12000
+bufferSize  = 1024
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
+
 Mesas = []
+clientMsg = ''
+message = ''
 comandos = ['Digite uma das opções','1 - cardápio', '2 - pedir', '3 - conta individual', '4 - pagar', '5 - levantar', '6 - conta da mesa']
+
 while 1:
-    comando, clientAddress = serverSocket.recvfrom(2048)
-    if comando.decode() == 'chefia':
-        serverSocket.sendto('Digite a mesa:'.encode(), clientAddress)
-        n_mesa, clientAddress = serverSocket.recvfrom(2048)
-        serverSocket.sendto('Digite seu nome:'.encode(), clientAddress)
-        nome, clientAddress = serverSocket.recvfrom(2048)
-        cliente = Cliente(nome.decode(), clientAddress)
-        if len(Mesas) == 0:
-            tMesa = Mesa(n_mesa)
-            tMesa.clientes.append(cliente)
-            Mesas.append(tMesa)
-        else:
-            for tMesa in Mesas:
-                if tMesa.numero == n_mesa:
-                    tMesa.clientes.append(cliente)
-                else:
-                    novaMesa = Mesa(n_mesa)
-                    novaMesa.clientes.append(cliente)
-                    Mesas.append(novaMesa)
+    message, clientAddress = serverSocket.recvfrom(bufferSize)
+    bytesAddressPair = serverSocket.recvfrom(bufferSize)
+    checkmessage = bytesAddressPair[0]
+    checkaddress = bytesAddressPair[1]
+    check = str(checksum(message)).encode()
+    error = 'error'
+    ok = 'ok'
+    while check != checkmessage:
+        serverSocket.send(error.encode(), clientAddress)
+        mens = serverSocket.recvfrom(bufferSize)
+        messsage = mens[0]
+        address = mens[1]
+        checkmessage, checkaddress = serverSocket.recvfrom(bufferSize)
+        check = str(checksum(message)).encode()
+
+    serverSocket.sendto(ok.encode(), checkaddress)
+    message, clientAddress = serverSocket.recvfrom(bufferSize)
+    while message.decode() != 'conta da mesa':
+        print(message.decode())
+        if message.decode() == 'chefia':
+            serverSocket.sendto('Digite a mesa:'.encode(), clientAddress)
+            n_mesa, clientAddress = serverSocket.recvfrom(bufferSize)
+            serverSocket.sendto('Digite seu nome:'.encode(), clientAddress)
+            nome, clientAddress = serverSocket.recvfrom(bufferSize)
+            cliente = Cliente(nome.decode(), clientAddress)
+            if len(Mesas) == 0:
+                tMesa = Mesa(n_mesa)
+                tMesa.clientes.append(cliente)
+                Mesas.append(tMesa)
+            else:
+                for tMesa in Mesas:
+                    if tMesa.numero == n_mesa:
+                        tMesa.clientes.append(cliente)
+                    else:
+                        novaMesa = Mesa(n_mesa)
+                        novaMesa.clientes.append(cliente)
+                        Mesas.append(novaMesa)
+
+
+
 
