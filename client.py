95b322ae5c19bd47d6164b99f4420ee6883e243c
Roberto Nunes <jrvn@cin.ufpe.br>
2022-04-18 14:44:17 -0300

    envio de arquivos

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+import time
 from struct import *
 from datetime import datetime
 from socket import *
+import os
 
 import utilidades
 from utilidades import *
@@ -10,6 +12,40 @@ serverName = 'localhost'
 serverPort = 12000
 bufferSize = 1024
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.settimeout(10.0)
+path = './arquivo.txt'
+
+#abre, em modo de leitura de byte, o arquivo a ser enviado
+file = open(path, 'r')
+
+#enviando nome do arquivo ao servidor
+
+clientSocket.sendto(createpkt(path, 0, serverPort), (serverName, serverPort))
+
+file_size = os.path.getsize(path)  # Size in bits
+pacote_em_kilobytes = 512
+pacote_em_bytes = pacote_em_kilobytes * 8
+
+#enviando numero de pacotes
+numero_de_pacotes = (file_size // pacote_em_bytes) + 14
+clientSocket.sendto(numero_de_pacotes.to_bytes(4, "little"), (serverName, serverPort))
+
+delay = 0.004
+tempo_estimado = numero_de_pacotes*(delay*1.2)
+
+for i in range(numero_de_pacotes):
+    dado = file.read(pacote_em_bytes)
+    pacote = createpkt(dado, i, serverPort)
+    clientSocket.sendto(pacote, (serverName, serverPort))
+    enviado = f"{int((i+1)*pacote_em_kilobytes)}/{int(pacote_em_kilobytes*numero_de_pacotes)}Kb"
+    print('\r'+enviado, end='')
+    time.sleep(delay)
+
+clientSocket.close()
+file.close()
+
+'''
 cmd = ''
 seq = 0
 while cmd != 'sair':
@@ -17,13 +53,14 @@ while cmd != 'sair':
     cmd = input(f'{apresentacao} Cliente:')
     comando = createpkt(cmd, seq, serverPort)
     clientSocket.sendto(comando, (serverName, serverPort))
-    ack, receive = udpextract(clientSocket.recvfrom(bufferSize)[0])
-
+    ack, nextack, receive = udpextract(clientSocket.recvfrom(bufferSize)[0])
+    print(receive)
     #recebe do servidor
 
-    while ACKresponse == b'0':
+    while ack == nextack:
         clientSocket.sendto(comando, (serverName, serverPort))
-    print(ACKresponse)
+    seq = seq + 1
 
 
 clientSocket.close()
+'''
--- server.py
+++ server.py
@@ -1,21 +1,62 @@
+import time
 from struct import *
 from socket import *
 from utilidades import *
 
 
 serverPort = 12000
-bufferSize = 1024
+bufferSize = 512
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
+serverSocket.settimeout(10.0)
+
+#recebendo nome do arquivo
+print("aguardando nome do arquivo a ser enviado")
+data, clientAddress = serverSocket.recvfrom(512)
+file_name = udpextract(data)[2]
+file_name = file_name.split('/')
+#while ack == nextack:
+#    pacote = createpkt('', ack, clientAddress[1])
+#    serverSocket.sendto(pacote, clientAddress)
+
+print("nome do arquivo recebido")
+#recebendo quantidade de pacotes
+data = serverSocket.recv(4)
+numero_de_pacotes = int.from_bytes(data, "little")
+
 comando = ''
 
+serverSocket.settimeout(5.0)
+file = open(f'./recebido/{file_name[1]}', "wb")
+pacote_em_kilobytes = 512
+pacote_em_bytes = pacote_em_kilobytes * 8
+
+print(f"Recebendo {numero_de_pacotes} pacotes...")
+start = time.time()
+for i in range(numero_de_pacotes):
+    data = serverSocket.recv(pacote_em_bytes+14)
+    ack, nextack, dado = udpextract(data)
+
+    porcentagem = f"Baixando... {round((100*(i+1))/numero_de_pacotes, 2)}%"
+    # print(porcentagem)
+    print('\r'+porcentagem, end='')
+
+tempo_de_download = round(time.time()-start, 2)
+print(f"\nO download foi completo em {tempo_de_download} sec")
+
+# Limpando buffers e sockets
+file.close()
+serverSocket.close()
+
+'''
 while comando != 'sair':
     message, clientAddress = serverSocket.recvfrom(bufferSize)
     ack, nextack, comando = udpextract(message)
     if ack == nextack:
-          pacote = createpkt(comando, ack, clientAddress[1])
+          pacote = createpkt('', ack, clientAddress[1])
           serverSocket.sendto(pacote, clientAddress)
     else:
         pacote = createpkt(comando, nextack, clientAddress[1])
         serverSocket.sendto(pacote, clientAddress)
+'''
