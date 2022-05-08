95514585b5ed0d5b9bfe24b4836fb048b2b3e356
hiagorns <hiagorns@gmail.com>
2022-05-08 14:16:41 -0300

    adicao do chechsum

--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 from socket import *
+from utilidades import *
 
 serverName = 'localhost'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = input("Insira uma mensagem em letras minúsculas: ")
+message = input("Insira a mensagem: ")
 
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print (modifiedMessage.decode())
-clientSocket.close
+clientSocket.close()
